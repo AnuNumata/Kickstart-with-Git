6805eab3112ab7a910f9bf2407a07a56651b3d5b
Anushreya <rao.anushreya@gmail.com>
2023-08-23 20:59:36 +0530

    Initial commit

---
+++ Git/sharp.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].style.fontWeight= 'bold';
+titles[0].style.color = 'green';
