49dd429288be7342f3951c0faaa6d439ae98ca40
Anushreya <rao.anushreya@gmail.com>
2023-09-30 23:54:11 +0530

    Initial commit

--- Git/index.js
+++ Git/index.js
@@ -1,11 +1,47 @@
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[0].style.fontWeight='bold';
-console.log(items[2]);
-items[1].style.fontWeight='bold';
-console.log(items[2]);
-items[2].style.fontWeight='bold';
-items[2].style.backgroundColor='green';
-console.log(items[3]);
-items[3].style.fontWeight='bold';
+var itemList= document.querySelector('#items');
+
+// Parent element 
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement); 
+//child nodes
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow';  
+//FirstChild
+console.log(itemList.firstChild);
+//firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='Hello 1';
+//lastchild
+console.log(itemList.lastChild);
+//lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='Hello 4';
+//nextSibling
+console.log(itemList.nextSibling);
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+//previousSibling
+console.log(itemList.previousSibling);
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color='green';
+//createElement
+// create a div
+var newDiv = document.createElement('div');
+//add class
+newDiv.className='hello';
+//add id
+newDiv.id='hello1'; 
+//add attr
+newDiv.setAttribute('title','Hello Div');
+//create text node
+var newDivText = document.createTextNode('Hello World');
+// add text to div
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize='30px';
+ container.insertBefore(newDiv, h1);
